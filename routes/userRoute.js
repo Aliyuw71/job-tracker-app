c6d7bfef1581e4e65c8c67c4cd278df62463625e
Wasiu Aliyu <aliyuw71@gmail.com>
2026-06-16 11:20:50 +0100

    Login limit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,7 +6,7 @@ const rateLimiter = require("express-rate-limit");
 
 const tryLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000,
-  max: 10,
+  max: 20,
   message: {
     msg: "Too many request from this IP, please try again after 15mins",
   },
